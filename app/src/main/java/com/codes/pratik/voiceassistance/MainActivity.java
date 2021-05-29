ec80b5bdd4d8a6d7dac9f27a5f86c7e1254f5641
pratikkate3 <pratikrkate@gmail.com>
2021-05-29 13:14:29 +0530

    Voice Recognizer Added

--- app/src/main/java/com/codes/pratik/voiceassistance/MainActivity.java
+++ app/src/main/java/com/codes/pratik/voiceassistance/MainActivity.java
@@ -2,13 +2,98 @@ package com.codes.pratik.voiceassistance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button speak;
+    TextView text;
+    SpeechRecognizer recognizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initializing views
+        initViews();
+
+        speak.setOnClickListener(v->{
+
+            Intent intent =  new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,1);
+            recognizer.startListening(intent);
+        });
+
+        recognizeVoice();
+    }
+
+    private void initViews() {
+        speak = findViewById(R.id.speak);
+        text = findViewById(R.id.textView);
+    }
+
+    private void recognizeVoice() {
+        if(SpeechRecognizer.isRecognitionAvailable(this)){
+            recognizer = SpeechRecognizer.createSpeechRecognizer(this);
+            recognizer.setRecognitionListener(new RecognitionListener() {
+                @Override
+                public void onReadyForSpeech(Bundle params) {
+
+                }
+
+                @Override
+                public void onBeginningOfSpeech() {
+
+                }
+
+                @Override
+                public void onRmsChanged(float rmsdB) {
+
+                }
+
+                @Override
+                public void onBufferReceived(byte[] buffer) {
+
+                }
+
+                @Override
+                public void onEndOfSpeech() {
+
+                }
+
+                @Override
+                public void onError(int error) {
+
+                }
+
+                @Override
+                public void onResults(Bundle results) {
+
+                    ArrayList<String> result = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                    text.setText(result.get(0));
+                }
+
+                @Override
+                public void onPartialResults(Bundle partialResults) {
+
+                }
+
+                @Override
+                public void onEvent(int eventType, Bundle params) {
+
+                }
+            });
+        }
     }
 }
